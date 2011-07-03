ce168f67b7b310e63f48a174be634675aece7f41
Ken <Ken@Ken2010.no-domain-set-bellcanada>
2011-07-03 00:26:09 -0400

    Removed gwt.  Server is now undeployable.

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/client/gwt/model/GWTCityType.java
+++
@@ -1,5 +0,0 @@
-package com.kenstevens.stratinit.client.gwt.model;
-
-public enum GWTCityType {
-	FORT, PORT, AIRPORT, TECH, BASE
-}
--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/client/gwt/status/StatusSetter.java
+++
@@ -1,7 +0,0 @@
-package com.kenstevens.stratinit.client.gwt.status;
-
-public interface StatusSetter {
-	public void setText(String text);
-
-	public void addText(String message);
-}
