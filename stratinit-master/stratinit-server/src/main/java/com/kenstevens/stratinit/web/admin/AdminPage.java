46732e9008e2183b74eb5f8ce573fd163f435694
Ken <fil512@users.noreply.github.com>
2011-07-04 00:59:59 -0400

    server shutdown working

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/web/admin/AdminPage.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/web/admin/AdminPage.java
@@ -1,16 +1,19 @@
 package com.kenstevens.stratinit.web.admin;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.kenstevens.stratinit.type.Constants;
 
 public class AdminPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
-    public AdminPage(final PageParameters parameters) {
+	public AdminPage(final PageParameters parameters) {
 		add(new Label("version", Constants.SERVER_VERSION));
-        // TODO Add your page's components here
-    }
+		add(new AdminForm("adminForm"));
+		FeedbackPanel feedbackPanel = new FeedbackPanel("feedback");
+		add(feedbackPanel);
+	}
 }
