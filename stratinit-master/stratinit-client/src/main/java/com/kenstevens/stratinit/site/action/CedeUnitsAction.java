740f6a8de471f251191a624f3c1d2a4032915482
Ken <Ken@localhost>
2011-07-06 09:22:34 -0400

    pulling in 1.1.46 changes

--- stratinit-master/stratinit-client/src/main/java/com/kenstevens/stratinit/site/action/CedeUnitsAction.java
+++ stratinit-master/stratinit-client/src/main/java/com/kenstevens/stratinit/site/action/CedeUnitsAction.java
@@ -12,12 +12,9 @@ import com.kenstevens.stratinit.model.Nation;
 import com.kenstevens.stratinit.model.NationView;
 import com.kenstevens.stratinit.model.UnitView;
 import com.kenstevens.stratinit.site.Action;
-import com.kenstevens.stratinit.site.ActionQueue;
 import com.kenstevens.stratinit.site.Command;
 import com.kenstevens.stratinit.site.command.CedeCommand;
 import com.kenstevens.stratinit.site.command.CedeUnitsCommand;
-import com.kenstevens.stratinit.ui.selection.SelectEvent;
-import com.kenstevens.stratinit.ui.selection.Selection.Source;
 import com.kenstevens.stratinit.util.Spring;
 
 @Scope("prototype")
@@ -25,10 +22,6 @@ import com.kenstevens.stratinit.util.Spring;
 public class CedeUnitsAction extends Action {
 	@Autowired
 	private Spring spring;
-	@Autowired
-	private ActionQueue actionQueue;
-	@Autowired
-	private SelectEvent selectEvent;
 	
 	private CedeCommand cedeCommand;
 
@@ -51,15 +44,4 @@ public class CedeUnitsAction extends Action {
 	public Command<? extends Object> getCommand() {
 		return cedeCommand;
 	}
-	
-	@Override
-	public void postRequest() {
-		super.postRequest();
-		if (actionQueue.isEmpty()) {
-			if (!units.isEmpty()) {
-				selectEvent.selectSectorCoords(units.get(0).getCoords(), Source.CANVAS_SELECT);
-			}
-		}
-	}
-
 }
