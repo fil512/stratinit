285dc54c59991aa63a2cfa9d627830ff28d9615f
Ken <Ken@localhost>
2011-07-06 09:19:48 -0400

    +  * Fixed exploit that allowed players to build a new city with less than 64 CP. +  * Fixed bug that wasn't listing units that gained mobility since last login in red +  * Fixed bug that crashed client when unit ceded

--- stratinit-master/stratinit-client/src/main/java/com/kenstevens/stratinit/ui/window/StatsWindow.java
+++ stratinit-master/stratinit-client/src/main/java/com/kenstevens/stratinit/ui/window/StatsWindow.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.kenstevens.stratinit.model.stats.Stats;
-import com.kenstevens.stratinit.move.WorldView;
 import com.kenstevens.stratinit.ui.shell.Window;
 
 @Component
