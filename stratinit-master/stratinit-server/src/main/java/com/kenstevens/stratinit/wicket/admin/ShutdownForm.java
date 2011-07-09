53d44260d3c4c244beb1bb85925caf2a3f75da09
Ken <Ken@localhost>
2011-07-09 00:10:44 -0400

    PostForm is working, but it didn't require admin login so it didnt post

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/admin/ShutdownForm.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/admin/ShutdownForm.java
@@ -1,29 +1,26 @@
 package com.kenstevens.stratinit.wicket.admin;
 
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.value.ValueMap;
 
 import com.kenstevens.stratinit.remote.None;
 import com.kenstevens.stratinit.remote.Result;
 import com.kenstevens.stratinit.remote.StratInit;
 import com.kenstevens.stratinit.wicket.util.InfoResult;
 
-public class ShutdownForm extends Form {
+public class ShutdownForm extends Form<ValueMap> {
 	@SpringBean
 	StratInit stratInit;
 	private static final long serialVersionUID = 1L;
 
-	@SuppressWarnings("serial")
 	public ShutdownForm(String id) {
 		super(id);
-
-		Button shutdownButton = new Button("shutdownButton") {
-			public void onSubmit() {
-				Result<None> result = stratInit.shutdown();
-				new InfoResult<None>(this).info(result);
-			}
-		};
-		add(shutdownButton);
 	}
+	
+    @Override
+    public final void onSubmit() {
+    	Result<None> result = stratInit.shutdown();
+    	new InfoResult<None>(this).info(result);
+    }
 }
