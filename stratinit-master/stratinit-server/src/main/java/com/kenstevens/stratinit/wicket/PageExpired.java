f7e66f0952df3a4220f1be038899a928a6b07891
Ken <Ken@localhost>
2011-07-16 19:20:57 -0400

    Wicketified remaining docs pages

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/PageExpired.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/PageExpired.java
@@ -3,6 +3,8 @@ package com.kenstevens.stratinit.wicket;
 import com.kenstevens.stratinit.wicket.components.HomeLink;
 
 public class PageExpired extends BasePage {
+	private static final long serialVersionUID = 1L;
+
 	public PageExpired() {
 		add(new HomeLink("homeLink"));
 	}
