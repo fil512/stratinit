b829b92a9b4415fc651da5ad1c1ecd8758402597
Ken <Ken@localhost>
2011-07-17 20:50:48 -0400

    removed other unneccessary dependencies

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/admin/AdminPage.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/admin/AdminPage.java
@@ -2,6 +2,7 @@ package com.kenstevens.stratinit.wicket.admin;
 
 import com.kenstevens.stratinit.wicket.BasePage;
 
+// FIXME test I need to login as admin
 public class AdminPage extends BasePage {
 	private static final long serialVersionUID = 1L;
 
