0a3414ae2447841476cff338a578d521bfb16b9a
kstevens <fil512@users.noreply.github.com>
2012-02-24 19:23:10 -0500

    fixed some links

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/HomePage.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/HomePage.java
@@ -4,14 +4,22 @@ import org.apache.wicket.Page;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
+import com.kenstevens.stratinit.wicket.docs.PlayPage;
+
 public class HomePage extends BasePage {
 	private static final long serialVersionUID = 1L;
 
     public HomePage() {
-    	add(new Label("username", getAuthenticatedSession().getUsername()));
+    	AuthenticatedSession authenticatedSession = getAuthenticatedSession();
+    	if (authenticatedSession.isSignedIn()) {
+    		add(new Label("username", authenticatedSession.getUsername()));
+    	} else {
+    		add(new Label("username", ""));
+    	}
 
 		// FIXME Really??
 		add(new BookmarkablePageLink<Page>("RegistrationPage2", RegistrationPage.class));
+		add(new BookmarkablePageLink<Page>("PlayPage2", PlayPage.class));
     }
 
 }
