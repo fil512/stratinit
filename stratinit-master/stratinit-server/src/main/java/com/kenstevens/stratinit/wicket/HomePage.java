f0ce1bd78c625e3a4309aff29361d3896961d39c
Ken <Ken@localhost>
2012-03-27 12:44:21 -0400

    moved videos off home page

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/HomePage.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/HomePage.java
@@ -5,6 +5,7 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 import com.kenstevens.stratinit.wicket.docs.PlayPage;
+import com.kenstevens.stratinit.wicket.docs.VideoPage;
 
 public class HomePage extends AuthenticatedPage {
 	private static final long serialVersionUID = 1L;
@@ -17,7 +18,8 @@ public class HomePage extends AuthenticatedPage {
     	}
 
 		add(new BookmarkablePageLink<Page>("RegistrationPage", RegistrationPage.class));
-		add(new BookmarkablePageLink<Page>("PlayPage2", PlayPage.class));
+		add(new BookmarkablePageLink<Page>("VideoPage", VideoPage.class));
+		add(new BookmarkablePageLink<Page>("PlayPage", PlayPage.class));
     }
 
 }
