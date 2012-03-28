411961536822aba0221a696be6763ebf139b3aa3
Ken <Ken@localhost>
2012-03-28 18:39:38 -0400

    preparing charts

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/games/PlayerUnitsPage.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/games/PlayerUnitsPage.java
@@ -1,7 +1,14 @@
 package com.kenstevens.stratinit.wicket.games;
 
+import java.awt.Dimension;
+
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.wicketstuff.googlecharts.AbstractChartData;
+import org.wicketstuff.googlecharts.Chart;
+import org.wicketstuff.googlecharts.ChartDataEncoding;
+import org.wicketstuff.googlecharts.ChartProvider;
+import org.wicketstuff.googlecharts.ChartType;
 
 import com.kenstevens.stratinit.wicket.BasePage;
 
@@ -18,6 +25,25 @@ public class PlayerUnitsPage extends BasePage {
 						"gameId").toInt(), pageParameters.get("name")
 						.toString()));
 		add(playerUnitsView);
+		Chart chart = getChart();
+		add(chart);
+	}
+
+	private Chart getChart() {
+		@SuppressWarnings("serial")
+		AbstractChartData data = new AbstractChartData(ChartDataEncoding.TEXT,
+				100) {
+
+			public double[][] getData() {
+				return new double[][] { { 100, 80, 60, 30, 30, 30, 10 } };
+			}
+		};
+
+		ChartProvider provider = new ChartProvider(new Dimension(200, 100),
+				ChartType.VENN, data);
+
+		Chart chart = new Chart("venn", provider);
+		return chart;
 	}
 
 }
