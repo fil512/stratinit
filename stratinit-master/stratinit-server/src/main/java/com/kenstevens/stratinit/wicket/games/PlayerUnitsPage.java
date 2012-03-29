d7bcd330a960c7b67f7fd747b22ca75059ec953c
Ken <Ken@localhost>
2012-03-28 22:31:58 -0400

    Getting close.  Just need to pass in array and chart is done.

--- stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/games/PlayerUnitsPage.java
+++ stratinit-master/stratinit-server/src/main/java/com/kenstevens/stratinit/wicket/games/PlayerUnitsPage.java
@@ -1,15 +1,14 @@
 package com.kenstevens.stratinit.wicket.games;
 
-import java.awt.Dimension;
+import java.util.Map;
 
+import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.wicketstuff.googlecharts.AbstractChartData;
-import org.wicketstuff.googlecharts.Chart;
-import org.wicketstuff.googlecharts.ChartDataEncoding;
-import org.wicketstuff.googlecharts.ChartProvider;
-import org.wicketstuff.googlecharts.ChartType;
+import org.apache.wicket.util.template.JavaScriptTemplate;
+import org.apache.wicket.util.template.PackageTextTemplate;
 
+import com.google.common.collect.Maps;
 import com.kenstevens.stratinit.wicket.BasePage;
 
 public class PlayerUnitsPage extends BasePage {
@@ -25,25 +24,19 @@ public class PlayerUnitsPage extends BasePage {
 						"gameId").toInt(), pageParameters.get("name")
 						.toString()));
 		add(playerUnitsView);
-		Chart chart = getChart();
-		add(chart);
 	}
-
-	private Chart getChart() {
-		@SuppressWarnings("serial")
-		AbstractChartData data = new AbstractChartData(ChartDataEncoding.TEXT,
-				100) {
-
-			public double[][] getData() {
-				return new double[][] { { 100, 80, 60, 30, 30, 30, 10 } };
-			}
-		};
-
-		ChartProvider provider = new ChartProvider(new Dimension(200, 100),
-				ChartType.VENN, data);
-
-		Chart chart = new Chart("venn", provider);
-		return chart;
+	public void renderHead(IHeaderResponse response) {
+		PackageTextTemplate ptTemplate = new PackageTextTemplate(this.getClass(), "playerUnitsChart.tpl");
+		JavaScriptTemplate jsTemplate = new JavaScriptTemplate(ptTemplate);
+		Map< String , Object > parameters = Maps.newHashMap();
+		parameters.put("title","Zoo Company Performance");
+		response.renderString(jsTemplate.asString(parameters));
+//		add(TextTemplateHeaderContributor.forJavaScript(jsTemplate, new  Model((Serializable) parameters)));
+//		Label getOptions = new Label(
+//				"getOptionsScript",
+//				"function getOptions() { return { title: 'Company Performance',	hAxis: {title: 'Year', titleTextStyle: {color: 'red'}}}}");
+//		getOptions.setEscapeModelStrings(false);
+//		add(getOptions);
 	}
 
 }
