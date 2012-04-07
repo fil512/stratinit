58a5cadceebe3eb9c178bfb21668f03d650ce2a4
kstevens <kstevens@laptopken.intware.com>
2012-04-07 01:02:33 -0400

    reduced complexity of unitsCede

--- stratinit-master/stratinit-remote/src/main/java/com/kenstevens/stratinit/server/remote/move/UnitsCede.java
+++ stratinit-master/stratinit-remote/src/main/java/com/kenstevens/stratinit/server/remote/move/UnitsCede.java
@@ -1,7 +1,6 @@
 package com.kenstevens.stratinit.server.remote.move;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.kenstevens.stratinit.dao.GameDao;
-import com.kenstevens.stratinit.dao.UnitDao;
 import com.kenstevens.stratinit.dto.SIUpdate;
 import com.kenstevens.stratinit.model.Nation;
 import com.kenstevens.stratinit.model.Relation;
@@ -35,8 +33,6 @@ public class UnitsCede {
 	private UnitDaoService unitDaoService;
 	@Autowired
 	private SectorDaoService sectorDaoService;
-	@Autowired
-	private UnitDao unitDao;
 
 	private final Nation nation;
 	private final List<Unit> units;
@@ -61,15 +57,37 @@ public class UnitsCede {
 		List<Unit> unitsToCede = new ArrayList<Unit>();
 		Nation target = gameDao.getNation(gameId, nationId);
 		
-		Relation relation = gameDao.findRelation(nation, target);
-		Relation reverse = gameDao.findRelation(target, nation);
-		if (relation.getType() != RelationType.ALLIED || reverse.getType() != RelationType.ALLIED) {
+		if (!checkAllied(target)) {
 			return new Result<SIUpdate>("You may only cede units to an ally.", false);
-		}
+		}		
 
 		SectorCoords coords = units.get(0).getCoords();
-		WorldSector worldSector = worldView.getWorldSector(coords);
+		
+		removeDistantUnits(unitsToCede, coords);
 
+		if (unitsToCede.isEmpty()) {
+			return new Result<SIUpdate>("Nothing to cede.", false);
+		}
+
+		Result<None> result = cedeUnits(unitsToCede, target);
+		sectorDaoService.survey(nation);
+		sectorDaoService.survey(target);
+		SIUpdate siupdate = playerWorldViewUpdate.getWorldViewUpdate(nation);
+
+		return new Result<SIUpdate>(result.getMessages(), true, siupdate,
+				result.getBattleLogs(), result.isSuccess());
+	}
+
+	private Result<None> cedeUnits(List<Unit> unitsToCede, Nation target) {
+		Result<None> result = Result.trueInstance();
+		for (Unit unitToCede : unitsToCede) {
+			result.or(unitDaoService.cedeUnit(unitToCede, target));
+		}
+		return result;
+	}
+
+	private void removeDistantUnits(List<Unit> unitsToCede, SectorCoords coords) {
+		WorldSector worldSector = worldView.getWorldSector(coords);
 		for (Unit unit : units) {
 			// Exclude units not in the same sector as the first unit
 			if (!unit.getCoords().equals(coords)) {
@@ -82,41 +100,14 @@ public class UnitsCede {
 
 			}
 		}
-
-		if (unitsToCede.isEmpty()) {
-			return new Result<SIUpdate>("Nothing to cede.", false);
-		}
-
-		Collection<Nation> allies = gameDao.getAllies(nation);
-		if (!allies.contains(target)
-				&& extraUnitsInSector(nation, coords, unitsToCede)) {
-			return new Result<SIUpdate>("Unable to cede units: You have other units in the same sector.", false);
-		}
-
-		Result<None> result = Result.trueInstance();
-		for (Unit unitToCede : unitsToCede) {
-			result.or(unitDaoService.cedeUnit(unitToCede, target));
-		}
-		sectorDaoService.survey(nation);
-		sectorDaoService.survey(target);
-		SIUpdate siupdate = playerWorldViewUpdate.getWorldViewUpdate(nation);
-
-		return new Result<SIUpdate>(result.getMessages(), true, siupdate,
-				result.getBattleLogs(), result.isSuccess());
 	}
 
-	private boolean extraUnitsInSector(Nation nation, SectorCoords coords,
-			List<Unit> unitsToCede) {
-
-		Collection<Unit> unitsInSector = unitDao.getUnits(nation.getGame(), coords);
-
-		boolean extraUnitsInSector = false;
-		for (Unit sectorUnit : unitsInSector) {
-			if (!unitsToCede.contains(sectorUnit)) {
-				extraUnitsInSector = true;
-			}
+	private boolean checkAllied(Nation target) {
+		Relation relation = gameDao.findRelation(nation, target);
+		Relation reverse = gameDao.findRelation(target, nation);
+		if (relation.getType() == RelationType.ALLIED && reverse.getType() == RelationType.ALLIED) {
+			return true;
 		}
-		return extraUnitsInSector;
+		return false;
 	}
-
 }
