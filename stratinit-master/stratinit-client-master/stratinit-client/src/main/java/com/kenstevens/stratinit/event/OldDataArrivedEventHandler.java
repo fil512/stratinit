20aca903ea2b63b260bff5c0641877e485c6be20
kstevens <fil512@users.noreply.github.com>
2012-04-10 22:25:54 -0400

    Replaced GWT HandlerManager with Guava EventBus

--- stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/event/OldDataArrivedEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.kenstevens.stratinit.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-@Deprecated
-public abstract class OldDataArrivedEventHandler implements EventHandler {
-	public abstract void dataArrived();
-}
