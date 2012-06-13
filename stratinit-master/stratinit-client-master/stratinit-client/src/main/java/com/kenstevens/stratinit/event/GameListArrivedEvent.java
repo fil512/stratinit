3e5b5ca4784a77ddea32aa086239d601a6a02faa
Ken <Ken@localhost>
2012-06-12 22:16:41 -0400

    Release 1.2.6   * Moved to install4j installation platform.   * If a user is in no games, start with the "Join Game" tab.

--- stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/event/GameListArrivedEvent.java
+++ stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/event/GameListArrivedEvent.java
@@ -3,4 +3,13 @@ package com.kenstevens.stratinit.event;
 
 
 public class GameListArrivedEvent implements DataArrivedEvent {
+	private final boolean joinedGames;
+
+	public GameListArrivedEvent(boolean joinedGames) {
+		this.joinedGames = joinedGames;
+	}
+
+	public boolean isJoinedGames() {
+		return joinedGames;
+	}
 }
--- stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/site/processor/UnjoinedGameListProcessor.java
+++ stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/site/processor/UnjoinedGameListProcessor.java
@@ -16,6 +16,6 @@ public class UnjoinedGameListProcessor extends Processor {
 	
 	public void process(List<SIGame> entries) {
 		db.getUnjoinedGameList().addAll(translator.translate(entries));
-		arrivedDataEventAccumulator.addEvent(new GameListArrivedEvent());
+		arrivedDataEventAccumulator.addEvent(new GameListArrivedEvent(false));
 	}
 }
