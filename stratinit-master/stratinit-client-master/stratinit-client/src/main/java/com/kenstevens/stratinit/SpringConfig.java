a3f127493ba877cdd2e724cf0813875b960a8dae
Ken Stevens <ken.stevens@sympatico.ca>
2021-02-15 16:23:03 -0500

    swt client connects to server and joins games

--- stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/SpringConfig.java
+++ stratinit-master/stratinit-client-master/stratinit-client/src/main/java/com/kenstevens/stratinit/SpringConfig.java
@@ -6,22 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
-import org.springframework.remoting.httpinvoker.HttpInvokerRequestExecutor;
 
 @Configuration
 @ComponentScan
 public class SpringConfig {
     @Bean
-    public HttpInvokerProxyFactoryBean invoker() {
+    public HttpInvokerProxyFactoryBean invoker(BasicAuthenticationCommonsHttpInvokerRequestExecutor executor) {
         HttpInvokerProxyFactoryBean invoker = new HttpInvokerProxyFactoryBean();
         invoker.setServiceUrl("http://localhost:8080/remote");
         invoker.setServiceInterface(StratInit.class);
-        invoker.setHttpInvokerRequestExecutor(httpInvoker());
+        invoker.setHttpInvokerRequestExecutor(executor);
         return invoker;
-    }
-
-    @Bean
-    public HttpInvokerRequestExecutor httpInvoker() {
-        return new BasicAuthenticationCommonsHttpInvokerRequestExecutor();
     }
 }
